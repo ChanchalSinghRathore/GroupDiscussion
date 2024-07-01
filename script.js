3a400462daf98cdb1011355dc7de6c54c763593d
ChanchalSinghRathore <chanchalrathore111003@gamil.com>
2024-07-01 20:05:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+
+// Add event listener to Get Started button
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".hero button").addEventListener("click", function() {
+        alert("Get Started button clicked!");
+    });
+});
+
